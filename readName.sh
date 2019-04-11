62b2d8c755688035f0a93a84ed4be54f9695f5bf
Navneet Nandan <nandanjha.navneet@gmail.com>
2019-04-12 00:06:28 +0530

    Create readName.sh

---
+++ readName.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Author : Navneet Nandan Jha
+# Copyright (c) navneet066
+# This script is for reading the name given by the user and printing the Welcome $name
+read name
+echo Welcome $name
