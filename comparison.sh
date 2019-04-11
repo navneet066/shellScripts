e7f4fe7f5af1b4a5db908fd9e5b1896b6d55b632
Navneet Nandan <nandanjha.navneet@gmail.com>
2019-04-12 00:09:59 +0530

    Create comparison.sh

---
+++ comparison.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Author : Navneet Nandan Jha
+# Copyright (c) navneet066
+read X
+read Y
+if [ $X -lt $Y ]; then
+        echo "X is less than Y"
+elif [ $X -gt $Y ]; then
+        echo "X is greater than Y"
+else
+        echo "X is equal to Y"
+fi
